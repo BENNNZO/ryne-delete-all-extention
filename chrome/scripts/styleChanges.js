6e6e43d50ed20c3aa11eb7798987d0336bb266d1
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-12 18:11:17 -0400

    styles ai and user chats!

--- chrome/scripts/styleChanges.js
+++ chrome/scripts/styleChanges.js
@@ -4,6 +4,7 @@ function setupStyles() {
     const style = document.createElement("style")
     style.id = "style-changes-styles"
     style.textContent = `
+        /* fix input area */
         .chat-main-bottom {
             background: rgba(0, 0, 0, 0.8);
             backdrop-filter: blur(5px);
@@ -11,18 +12,79 @@ function setupStyles() {
             border-top: 1px solid rgba(255, 255, 255, 0.25);
         }
 
-        .chat-bot {
-            backdrop-filter: blur(5px);
-        }
-
-        .chat-messages + div {
-            display: none;
-        }
-
+        /* fix nav bar */
         header {
             border: none !important;
             border-top: 1px solid rgba(255, 255, 255, 0.25) !important;
             padding-right: 4px;
+        }
+
+        /* remove intro welcome hero */
+        .chat-messages + div {
+            display: none;
+        }
+
+        /* add gap to message column */
+        .chat-messages {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+
+        /* style ai answers */
+        .chat-bot {
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            backdrop-filter: blur(5px);
+            margin-bottom: 0;
+        }
+
+        .chat-bot > :first-child {
+            display: none;
+        }
+
+        .chat-bot > :nth-child(2) {
+            max-width: 100%;
+            margin-left: 0;
+        }
+
+        /* style user prompts */
+        .chat:not(.chat-bot) {
+            border-radius: 15px;
+
+            width: 90%;
+            margin-left: auto;
+
+            padding: 10px 15px;
+        }
+
+        .chat:not(.chat-bot) > div:nth-of-type(1) {
+            /* remove user profile image */
+            display: none;
+        }
+
+        .chat:not(.chat-bot) > div:nth-of-type(2) {
+            /* position user prompt text */
+            margin-left: auto;
+        }
+
+        .chat:not(.chat-bot) > div:nth-of-type(2) > div:nth-of-type(1) {
+            /* remove random margin element */
+            display: none;
+        }
+
+        /* style code sections to remove wrapping issue */
+        pre {
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            padding: 0 !important;
+        }
+
+        pre code {
+            display: inline-block;
+            white-space: nowrap !important;
+        }
+
+        pre  {
+            overflow-x: scroll;
         }
     `
 
@@ -34,7 +96,7 @@ function main() {
         return
     } else {
         console.log("[Ryne Toolkit] Initializing Style Changes...")
-        
+
         window.__styleChangesInitialized = true
 
         setupStyles()
