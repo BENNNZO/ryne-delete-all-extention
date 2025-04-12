9aaad23d507de84b13140d65f0b082de7757a5f0
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-12 16:20:36 -0400

    init check

--- chrome/scripts/styleChanges.js
+++ chrome/scripts/styleChanges.js
@@ -1,1 +1,11 @@
-// placeholder
+function main() {
+    if (window.__styleChangesInitialized) {
+        return
+    } else {
+        console.log("[Ryne Toolkit] Initializing Style Changes...")
+        
+        window.__styleChangesInitialized = true
+    }
+}
+
+main()
