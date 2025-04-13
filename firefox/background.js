23042616d14257d56f296bda8580fee5db84e116
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-12 20:38:54 -0400

    copied over files

---
+++ firefox/background.js
@@ -0,0 +1,30 @@
+chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
+    if (tab.url.includes("https://ryne.ai") && info.status === "complete") {
+        chrome.storage.sync.get(["historyCleaner"]).then(res => {
+            if (res.historyCleaner) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    files: ["/scripts/historyCleaner.js"]
+                })
+            }
+        })
+        
+        chrome.storage.sync.get(["autoFocus"]).then(res => {
+            if (res.autoFocus) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    files: ["/scripts/autoFocus.js"]
+                })
+            }
+        })
+        
+        chrome.storage.sync.get(["styleChanges"]).then(res => {
+            if (res.styleChanges) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    files: ["/scripts/styleChanges.js"]
+                })
+            }
+        })
+    }
+})
---
+++ firefox/scripts/historyCleaner.js
@@ -0,0 +1,72 @@
+function setupStyles() {
+    if (document.getElementById("delete-all-button-styles")) return
+
+    const style = document.createElement("style")
+    style.id = "delete-all-button-styles"
+    style.textContent = `
+        .delete-all-button {
+            background: black; 
+            color: rgb(229, 231, 235); 
+            border-radius: 0.75rem; 
+            padding: 0rem 1rem; 
+            border: 1px solid rgb(31, 41, 55);
+            transition: ease-in-out 150ms
+        }
+
+        .delete-all-button:hover {
+            border-color: rgb(55, 65, 81);
+        }
+    `
+
+    document.head.appendChild(style)
+}
+
+function injectButton(target) {
+    if (document.querySelector(".delete-all-button")) return
+
+    const container = target.querySelector(".main-container-history > .mt-4")
+    container.style = "display: flex; gap: 2px"
+
+    const button = document.createElement("button")
+    button.classList.add("delete-all-button")
+    button.innerText = "DEL"
+    button.addEventListener("click", () => {
+        const deleteButtons = document.querySelectorAll(".cursor-pointer.history-rise-shake.text-rose-500")
+
+        deleteButtons.forEach(deleteButton => {
+            deleteButton.dispatchEvent(new MouseEvent("click", { bubbles: true, view: window, cancelable: true, shiftKey: true }))
+        })
+
+        location.reload()
+    })
+
+    container.appendChild(button)
+}
+
+function setupObserver() {
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(node => {
+                if (node.querySelector(".main-container-history")) injectButton(node)
+            })
+        })
+    })
+
+    observer.observe(document.body, { childList: true })
+}
+
+function main() {
+    if (window.__historyCleanerInitialized) {
+        return
+    } else {
+        console.log("[Ryne Toolkit] Initializing History Cleaner...")
+
+        window.__historyCleanerInitialized = true
+
+        setupStyles()
+        setupObserver()
+    }
+
+}
+
+main()
