6dbb2b55f334f08655f6b63ae11ff7899d28f2f8
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-12 20:48:48 -0400

    ported to manifest v2

--- firefox/background.js
+++ firefox/background.js
@@ -2,27 +2,24 @@ browser.tabs.onUpdated.addListener((tabId, info, tab) => {
     if (tab.url.includes("https://ryne.ai") && info.status === "complete") {
         browser.storage.sync.get(["historyCleaner"]).then(res => {
             if (res.historyCleaner) {
-                browser.scripting.executeScript({
-                    target: { tabId: tab.id },
-                    files: ["/scripts/historyCleaner.js"]
+                browser.tabs.executeScript(tab.id, {
+                    file: "/scripts/historyCleaner.js"
                 })
             }
         })
         
         browser.storage.sync.get(["autoFocus"]).then(res => {
             if (res.autoFocus) {
-                browser.scripting.executeScript({
-                    target: { tabId: tab.id },
-                    files: ["/scripts/autoFocus.js"]
+                browser.tabs.executeScript(tab.id, {
+                    file: "/scripts/autoFocus.js"
                 })
             }
         })
         
         browser.storage.sync.get(["styleChanges"]).then(res => {
             if (res.styleChanges) {
-                browser.scripting.executeScript({
-                    target: { tabId: tab.id },
-                    files: ["/scripts/styleChanges.js"]
+                browser.tabs.executeScript(tab.id, {
+                    file: "/scripts/styleChanges.js"
                 })
             }
         })
