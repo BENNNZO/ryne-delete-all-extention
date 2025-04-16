1edc4ba512653e72d17eaf2e6054d7f6e7885243
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-16 19:10:25 -0400

    added button and a border around chat-input elements

--- chrome/scripts/quickActions.js
+++ chrome/scripts/quickActions.js
@@ -1,3 +1,49 @@
+function setupStyles() {
+    if (document.getElementById("ryne-toolkit-new-chat-button-styles")) return
+
+    const style = document.createElement("style")
+    style.id = "ryne-toolkit-new-chat-button-styles"
+    style.textContent = `
+        .chat-input-box > div:nth-of-type(1) > button {
+            border: 1px solid rgba(255, 255, 255, 0.08)
+        }
+
+        .chat-input-box + div > div > * {
+            border: 1px solid rgba(255, 255, 255, 0.08)
+        }
+
+        .ryne-toolkit-new-chat-button {
+            background: #090A1A;
+            padding: 0.375rem;
+            border-radius: 0.5rem;
+            border: 1px solid rgba(255, 255, 255, 0.08)
+        }
+
+        .ryne-toolkit-new-chat-button:hover {
+            background: #1A1C2C;
+        }
+    `
+
+
+    document.head.appendChild(style)
+}
+
+function setupHumanizerButton() {
+
+}
+
+function setupNewChatButton() {
+    const chatInputBoxButtonsContainer = document.querySelector(".chat-input-box").nextSibling.firstChild
+
+    const newChatButton = document.createElement('button')
+    newChatButton.classList.add("ryne-toolkit-new-chat-button")
+    newChatButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" width="20px" height="20px"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M256 112v288M400 256H112"/></svg>`
+
+    chatInputBoxButtonsContainer.insertBefore(newChatButton, chatInputBoxButtonsContainer.childNodes[0])
+    console.log(chatInputBoxButtonsContainer)
+
+}
+
 function main() {
     if (window.__quickActionsInitialized) {
         return
@@ -5,6 +51,11 @@ function main() {
         console.log("[Ryne Toolkit] Initializing Quick Actions...")
 
         window.__quickActionsInitialized = true
+
+        setupNewChatButton()
+        setupHumanizerButton()
+
+        setupStyles()
     }
 }
 
